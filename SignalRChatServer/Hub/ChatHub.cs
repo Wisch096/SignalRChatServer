f31497a4f65ae21571c68085bd66ff6b25e5048e
Matheus Wisch <wisch.pg@outlook.com>
2024-06-15 15:42:23 -0300

    feat :sparkles: create .gitignore

--- SignalRChatServer/Hub/ChatHub.cs
+++ SignalRChatServer/Hub/ChatHub.cs
@@ -5,5 +5,5 @@ namespace SignalRWebpack.Hubs;
 public class ChatHub : Hub
 {
     public async Task NewMessage(long username, string message) =>
-       a await Clients.All.SendAsync("messageReceived", username, message);
+        await Clients.All.SendAsync("messageReceived", username, message);
 }
